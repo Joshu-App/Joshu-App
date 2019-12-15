71b42b4cf80b89c7a5941f99b044edb55daa0185
Alba Lamas <alba.v.lamas@gmail.com>
2019-12-15 10:23:22 +0100

    descr

---
+++ src/app/ship-description/ship-description.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ship-description',
+  templateUrl: './ship-description.component.html',
+  styleUrls: ['./ship-description.component.css']
+})
+export class CharacterDescriptionComponent implements OnInit {
+
+  constructor() { }
+
+  name: string;
+  fuerza: number = 20
+  velocidad: number = 70
+  inteligencia : number = 100
+
+  ngOnInit() {
+    this.name = "Casimiro"
+    this.fuerza = 20;
+    this.velocidad = 70;
+    this.inteligencia = 100
+  }
+
+}
---
+++ src/app/ship-image/ship-image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ship-image',
+  templateUrl: './ship-image.component.html',
+  styleUrls: ['./ship-image.component.css']
+})
+export class ShipImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
